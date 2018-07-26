a053072a11c21db023e11367717bb1ff12567ffc
QuantumSheep <quantumsheep@outlook.fr>
2018-07-26 12:30:14 +0200

    Format articles list with some conditions

--- client/src/components/Articles/Articles.js
+++ client/src/components/Articles/Articles.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import socket from '../../socket';
 import './Articles.css';
+import PropTypes from 'prop-types';
+import withWidth from '@material-ui/core/withWidth';
 import Article from '../Article/Article';
 import Grid from '@material-ui/core/Grid';
 
 class Articles extends Component {
+  static propTypes = {
+    width: PropTypes.string.isRequired,
+  }
+
   constructor(props) {
     super(props);
 
@@ -13,11 +19,9 @@ class Articles extends Component {
     }
 
     socket.on('new article', article => {
-      console.log(article);
-
       this.state.articles.push(article);
 
-      this.setState(this.state);
+      this.setState({ articles: this.state.articles });
     });
   }
 
@@ -25,15 +29,27 @@ class Articles extends Component {
     return (
       <Grid container justify="center">
         {
-          this.state.articles.map(article => (
-            <Grid item xs={12} key={article.id}>
-              <Article id={article.id} title={article.title} description={article.description} />
-            </Grid>
-          ))
+          this.state.articles.map((article, index) => {
+            if (index < 6) {
+              const padding = this.props.width !== 'xs' ? (index % 2 ? { paddingLeft: 7.5 } : { paddingRight: 7.5 }) : {};
+              
+              return (
+                <Grid item xs={12} sm={6} key={article.id} style={padding}>
+                  <Article id={article.id} title={article.title} description={article.description} />
+                </Grid>
+              );
+            } else {
+              return (
+                <Grid item xs={12} key={article.id}>
+                  <Article id={article.id} title={article.title} description={article.description} />
+                </Grid>
+              );
+            }
+          })
         }
       </Grid>
     );
   }
 }
 
-export default Articles;
+export default withWidth()(Articles);
