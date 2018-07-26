4ae4eac088a9e9f7bdb98e3cddee9bc11616cb55
QuantumSheep <nathanael.dmc@outlook.fr>
2018-07-26 02:00:27 +0200

    Implement login dialog with login button

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,20 +5,31 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import config from '../../config';
 import './Navbar.css';
+import LoginDialog from '../LoginDialog/LoginDialog';
 
 class Navbar extends Component {
+  state = {
+    dialogOpen: false
+  }
+
   render() {
     return (
-      <AppBar position="static">
-        <Toolbar className="Navbar">
-          <Typography variant="title" color="inherit" className="logo">
-            {config.title}
-          </Typography>
-          <div>
-            <Button color="inherit">Login</Button>
-          </div>
-        </Toolbar>
-      </AppBar>
+      <div>
+        <AppBar position="static" color="default" className="NavbarContainer">
+          <Toolbar variant="dense" className="Navbar">
+            <Typography variant="title" color="inherit" className="logo">
+              {config.title}
+            </Typography>
+            <div>
+              <Button color="inherit" onClick={() => this.setState({ dialogOpen: true })}>Login</Button>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <LoginDialog
+          open={this.state.dialogOpen}
+          onClose={() => this.setState({ dialogOpen: false })}
+        />
+      </div>
     );
   }
 }
