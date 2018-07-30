bc1846dcb7791d43b5535fdc14911dbddbc84819
QuantumSheep <quantumsheep@outlook.fr>
2018-07-30 17:28:01 +0200

    Finalize signup action and add login action

--- server/controllers/action.js
+++ server/controllers/action.js
@@ -13,7 +13,7 @@ exports.signup = (req, res) => {
     /**
      * @type {Promise<string[]>}
      */
-    const process = new Promise((resolve, reject) => {
+    const action = new Promise((resolve, reject) => {
         if (req.body.username && req.body.email && req.body.password) {
             const verification = {
                 isUsernameError: !(req.body.username && req.body.username.length >= 3),
@@ -29,45 +29,57 @@ exports.signup = (req, res) => {
                     ]
                 });
 
-                findSimilarAccounts.then(accounts => {
-                    if (accounts.length > 0) {
-                        const errors = [];
+                findSimilarAccounts
+                    .then(accounts => {
+                        if (accounts.length > 0) {
+                            const errors = [];
 
-                        accounts.forEach(account => {
-                            if (account.username === req.body.username) {
-                                errors.push('There is already an account with this username.');
-                            }
+                            accounts.forEach(account => {
+                                if (account.username === req.body.username) {
+                                    errors.push('There is already an account with this username.');
+                                }
 
-                            if (account.email === req.body.email) {
-                                errors.push('There is already an account with this email.');
-                            }
-                        });
-
-                        reject(errors);
-                    } else {
-                        bcrypt.hash(req.body.password, 10, (err, encryptedPassword) => {
-                            const account = new AccountModel({
-                                username: req.body.username,
-                                email: req.body.email,
-                                password: encryptedPassword,
+                                if (account.email === req.body.email) {
+                                    errors.push('There is already an account with this email.');
+                                }
                             });
 
-                            account.save()
-                                .then(() => {
-                                    console.log(`New account: ${req.body.username} <${req.body.email}>`);
+                            reject(errors);
+                        } else {
+                            resolve();
+
+                            const encryptPassword = bcrypt.hash(req.body.password, 10);
+
+                            encryptPassword
+                                .then(encryptedPassword => {
+                                    const account = new AccountModel({
+                                        username: req.body.username,
+                                        email: req.body.email,
+                                        password: encryptedPassword,
+                                    });
+
+                                    const accountSaving = account.save();
+
+                                    accountSaving
+                                        .then(() => {
+                                            console.log(`New account: ${req.body.username} <${req.body.email}>`);
+                                        })
+                                        .catch(err => {
+                                            console.log(err);
+                                        });
                                 })
                                 .catch(err => {
                                     console.log(err);
+
+                                    reject(["An error occured."]);
                                 });
-                        });
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err);
 
-                        resolve();
-                    }
-                });
-
-                findSimilarAccounts.catch(err => {
-                    console.log(err);
-                });
+                        reject(["An error occured."]);
+                    })
             } else {
                 reject(["Inputs doesn't match the requirements."]);
             }
@@ -76,7 +88,79 @@ exports.signup = (req, res) => {
         }
     });
 
-    process
+    action
+        .then(() => {
+            res.end(JSON.stringify({
+                success: true
+            }));
+        })
+        .catch(err => {
+            res.end(JSON.stringify({
+                success: false,
+                errors: err
+            }));
+        });
+};
+
+/**
+ * Login action
+ * 
+ * @param {express.Request} req 
+ * @param {express.Response} res 
+ */
+exports.login = (req, res) => {
+    /**
+     * @type {Promise<string[]>}
+     */
+    const action = new Promise((resolve, reject) => {
+        if (req.body.email && req.body.password) {
+            const verification = {
+                isEmailError: !(req.body.email && req.body.email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)),
+                isPasswordError: !(req.body.password && req.body.password.length >= 8),
+            };
+
+            if (!verification.isEmailError && !verification.isPasswordError) {
+                const findSimilarAccounts = AccountModel.findOne({
+                    email: req.body.email
+                }, "password");
+
+                const credentialsError = "This credentials doesn't match with an existing account or the account doesn't exists.";
+
+                findSimilarAccounts
+                    .then(account => {
+                        if (!account) {
+                            reject([credentialsError]);
+                        } else {
+                            const checkPassword = bcrypt.compare(req.body.password, account.password)
+
+                            checkPassword
+                                .then(same => {
+                                    if (same) {
+                                        resolve();
+                                    } else {
+                                        reject([credentialsError]);
+                                    }
+                                })
+                                .catch(err => {
+                                    console.log(err);
+
+                                    reject([credentialsError]);
+                                });
+                        }
+                    }).catch(err => {
+                        console.log(err);
+
+                        reject(["An error occured."]);
+                    });
+            } else {
+                reject(["Inputs doesn't match the requirements."]);
+            }
+        } else {
+            reject(['Please complete the form before sending it.']);
+        }
+    });
+
+    action
         .then(() => {
             res.end(JSON.stringify({
                 success: true
--- server/routes/action.js
+++ server/routes/action.js
@@ -2,10 +2,12 @@ const express = require('express');
 const action = require('../controllers/action');
 
 /**
- * Index routes **(index, login, signup)**
+ * Action routes **(login, signup)**
  * 
  * @param {express.Express} app 
  */
 module.exports = app => {
+    app.post('/action/login', action.login);
+
     app.post('/action/signup', action.signup);
 }
