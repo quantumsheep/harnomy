29fafe64391b3d594427458f190c4596faabc7be
QuantumSheep <quantumsheep@outlook.fr>
2018-07-30 17:26:51 +0200

    Add sessions

--- server/server.js
+++ server/server.js
@@ -9,6 +9,18 @@ const helmet = require('helmet');
 const router = require('./routes/router');
 const sockethandler = require('./services/sockethandler');
 
+const db = require('./services/db');
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+
+app.use(session({
+    secret: `vg*\\ZLM>qYPet^T>XQ#^2.=vGV'x/c=ZP5P6-9;'P.2w=Gm&Lq-{&c\\Js@@-jL^^\\JFtTDj}m2D/B9eK8#6j5^spN7%>4mUvD%mbY.E2-7Zp*XRWW}JbFp4\\C?h:Q3+`,
+    store: new MongoStore({ mongooseConnection: db.connection }),
+    resave: true,
+    saveUninitialized: true,
+}));
+
 const config = {
     enableCORS: true
 };
